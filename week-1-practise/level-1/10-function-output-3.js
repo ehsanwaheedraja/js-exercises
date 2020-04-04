720a59664039fe75c78fc7022888c23ac6621df2
ehsanwaheedraja <ihsan_raja@live.com>
2020-04-04 02:02:12 +0200

    solved untill level 1

--- week-1-practise/level-1/10-function-output-3.js
+++ week-1-practise/level-1/10-function-output-3.js
@@ -1,6 +1,13 @@
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
+    return firstWord.concat(secondWord).concat(thirdWord);
 }
+/*function s(w1, w2) {
+    return w1.concat(w2);
+}
+
+var result = s('code', 'your');
+console.log(result);*/
 
 var result = concatenate('code', 'your', 'future');
 console.log(result); // expected result = "codeyourfuture"
--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+//it will generate a random number
 // - How many parameters does the function take?
+// zero
 // - What is the function name?
+//getNumber
 // - Where is the function called? with what parameters?
+// it is called to get a value for variable "result".
